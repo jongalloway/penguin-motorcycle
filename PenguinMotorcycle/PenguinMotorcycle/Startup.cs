087151cf7dbb69b83017f379406266c4f8579b7a
Jon Galloway <jongalloway@gmail.com>
2015-01-14 12:51:51 -0800

    Initial commit! So excite!

---
+++ PenguinMotorcycle/PenguinMotorcycle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PenguinMotorcycle.Startup))]
+namespace PenguinMotorcycle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
